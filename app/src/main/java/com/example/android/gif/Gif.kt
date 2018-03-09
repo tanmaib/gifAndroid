d01448d3fe2cbf1c457a5efbc9128af495547923
tanmaibhargav <tanmaibhargav@gmail.com>
2018-03-09 17:16:43 +0530

    Add a gif in the UI.

---
+++ app/src/main/java/com/example/android/gif/Gif.kt
@@ -0,0 +1,12 @@
+package com.example.android.gif
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Gif : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gif)
+    }
+}
